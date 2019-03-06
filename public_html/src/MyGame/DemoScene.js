fd252e3be718d54baf17aafa5fcdcf223a9083be
Tod Kahler <kahlewil@uw.edu>
2019-03-05 19:18:07 -0800

    Added scene from json support
    as of rn only, cameras, patrol, cannon, flier, powerup, sceneBG, storyPanels

--- public_html/src/MyGame/DemoScene.js
+++ public_html/src/MyGame/DemoScene.js
@@ -1,5 +1,5 @@
 /*
- * File: TacoDemo.js
+ * File: DemoScene.js
  * Main file that we will use to test our Taco game DEMO
  *
  */
@@ -12,7 +12,7 @@
 
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
-function TacoDemo() {
+function DemoScene() {
     // remember that assets size must be in power of 2
     this.kPlatformTexture = "assets/Taco/platform.png";
     this.kKelvin = "assets/Taco/kelvinSpriteRun.png";
@@ -22,7 +22,7 @@ function TacoDemo() {
     this.kHealthBar = "assets/UI/healthbar.png";
     this.kWBPanel = "assets/Taco/WornWhiteboard.png";
     this.kGreenPipe = "assets/Taco/GreenPipe.png";
-
+    this.kSceneFile = "assets/Taco/DemoScene.json";
     // The camera to view the scene
     this.mCamera = null;
     this.mMinimapCam = null;
@@ -49,9 +49,9 @@ function TacoDemo() {
     this.MainMenuButton = null;
 
 }
-gEngine.Core.inheritPrototype(TacoDemo, Scene);
+gEngine.Core.inheritPrototype(DemoScene, Scene);
 
-TacoDemo.prototype.loadScene = function () {
+DemoScene.prototype.loadScene = function () {
     gEngine.Textures.loadTexture(this.kPlatformTexture);
     gEngine.Textures.loadTexture(this.kKelvin);
     gEngine.Textures.loadTexture(this.kBG);
@@ -60,10 +60,11 @@ TacoDemo.prototype.loadScene = function () {
     gEngine.Textures.loadTexture(this.kHealthBar);
     gEngine.Textures.loadTexture(this.kWBPanel);
     gEngine.Textures.loadTexture(this.kGreenPipe);
+    gEngine.TextFileLoader.loadTextFile(this.kSceneFile, gEngine.TextFileLoader.eTextFileType.eTextFile);
     //document.getElementById("particle").style.display="block"; //display the instruction below
 };
 
-TacoDemo.prototype.unloadScene = function () {
+DemoScene.prototype.unloadScene = function () {
     gEngine.Textures.unloadTexture(this.kPlatformTexture);
     gEngine.Textures.unloadTexture(this.kKelvin);
     gEngine.Textures.unloadTexture(this.kBG);
@@ -72,6 +73,7 @@ TacoDemo.prototype.unloadScene = function () {
     gEngine.Textures.unloadTexture(this.kHealthBar);
     gEngine.Textures.unloadTexture(this.kWBPanel);
     gEngine.Textures.unloadTexture(this.kGreenPipe);
+    gEngine.TextFileLoader.unloadTextFile(this.kSceneFile);
     //document.getElementById("particle").style.display="none";
     if(this.LevelSelect==="Back")
         gEngine.Core.startScene(new MyGame());
@@ -81,67 +83,37 @@ TacoDemo.prototype.unloadScene = function () {
         gEngine.Core.startScene(new WinScene());
     else if(this.LevelSelect==="Lose")
         gEngine.Core.startScene(new LoseScene());
-    else if (this.LevelSelect==="DemoScene") {
-        gEngine.Core.startScene(new DemoScene());
-    }
+    
 };
 
-TacoDemo.prototype.initialize = function () {
+DemoScene.prototype.initialize = function () {
     // Step A: set up the cameras
-    this.mCamera = new Camera(
-        vec2.fromValues(50, 36), // position of the camera
-        100,                     // width of camera
-        [0, 0, 800, 600]         // viewport (orgX, orgY, width, height)
-    );
-    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
-
-    this.mMinimapCam = new Camera(
-        vec2.fromValues(50, 26), // position of the camera
-        100,                     // width of camera
-        [600, 420, 200, 100]         // viewport (orgX, orgY, width, height)
-    );
-    this.mMinimapCam.setBackgroundColor([0.2, 0.8, 0.4, 1]);
-
-    gEngine.DefaultResources.setGlobalAmbientIntensity(3); // game brightness
-    gEngine.Physics.incRelaxationCount(15); //time to rest after a physics event
-
     this.mAllNonPhysObj = new GameObjectSet(); // contains all non-physics objects (bullets)
     this.mAllObjs = new GameObjectSet();    // store all physics object
     this.mAllPlatform = new GameObjectSet(); //store all platform
+    
+    // kelvin with set animation
+    this.mKelvin = new Hero(this.kKelvin, 10, 10, null);
+    this.mAllObjs.addToSet(this.mKelvin);
+    
+    var jsonString = gEngine.ResourceMap.retrieveAsset(this.kSceneFile);
+    var sceneInfo = JSON.parse(jsonString); 
+    this.parseObjects(sceneInfo);
+    
+    
+    var cams = sceneInfo.Camera;   
+    this.mCamera = this.parseCamera(cams[0]); 
+    this.mMinimapCam = this.parseCamera(cams[1]);
+
+
+    gEngine.DefaultResources.setGlobalAmbientIntensity(3); // game brightness
+    gEngine.Physics.incRelaxationCount(15); //time to rest after a physics event
 
     // make the bounds.. platform etc
     this.createBounds();
     this.mPipe = this.createPipe();
 
-    // kelvin with set animation
-    this.mKelvin = new Hero(this.kKelvin, 10, 10, null);
-    this.mAllObjs.addToSet(this.mKelvin);
-
-    // init Patrol
-    this.mPatrol = new Patrol(this.kSprites, 35, 19, this.mKelvin);
-    this.mAllObjs.addToSet(this.mPatrol);
-
-    // init cannon
-    this.mCannons = new Cannon(this.kSprites, 85, 19.5, this.mKelvin, this.mAllNonPhysObj);
-    this.mAllObjs.addToSet(this.mCannons);
-
-    this.mFlier = new Flier(this.kSprites, 70, 36, this.mKelvin, this.mAllNonPhysObj);
-    this.mAllNonPhysObj.addToSet(this.mFlier);
-
-    this.mPowerup = new Powerup(this.kSprites, 20, 30, this.mKelvin);
-    this.mAllNonPhysObj.addToSet(this.mPowerup);
-
-    // scene background
-    this.mSceneBG = new TextureRenderable(this.kBG);
-    this.mSceneBG.getXform().setSize(100,50);
-    this.mSceneBG.getXform().setPosition(50,26);
-
-    // tutorial panel. @param(texture,atX,atY,width,txt,stubX,stubY)
-    this.mTutoPanel = new StoryPanel(this.kWBPanel,50,20,70,
-        "Story Element Panel Demo",15,3);
-    this.mTutoPanel.setText2("Space to jump. S to stomp. E to super.");
-    this.mTutoPanel.setText3("Take the health pack above.");
-    this.mTutoPanel.setText4("Solve the code and warp down to win.");
+    
 
     // the code box to unlock green pipe
     //@param [atX,atY,w,stubX,stubY,code]
@@ -158,9 +130,125 @@ TacoDemo.prototype.initialize = function () {
     this.MainMenuButton = new UIButton(this.kUIButton,this.mainSelect,this,[700,576],[200,40],"Main Menu",4,[1,1,1,1],[1,1,1,1]);
 };
 
+
+DemoScene.prototype.parseCamera = function (camInfo) {
+    
+    
+    var cxy = camInfo.Center;
+    
+    var cx = Number(cxy[0]);
+    var cy = Number(cxy[1]);
+    var w = Number(camInfo.Width);
+    
+    var i;
+    var viewport = [1,1,1,1];
+    
+    for (i = 0; i < 4; i++) {
+
+        viewport[i] = Number(camInfo.Viewport[i]);
+    }
+    i = 0;
+    var bgColor = [1,1,1,1];
+    for (i = 0; i < 4; i++) {
+        bgColor[i] = Number(camInfo.BgColor[i]);
+    }
+    
+
+    var cam = new Camera(
+        vec2.fromValues(cx, cy),  // position of the camera
+        w,                        // width of camera
+        viewport                  // viewport (orgX, orgY, width, height)
+        );
+    cam.setBackgroundColor(bgColor);
+    return cam;
+};
+
+DemoScene.prototype.parseObjects = function (sceneInfo) {
+    var patrols = sceneInfo.Patrol;
+    var i, pos, patrol;
+    for (i = 0; i < patrols.length; i++) {
+        pos = patrols[i].Pos;
+        patrol = new Patrol(this.kSprites, pos[0], pos[1], this.mKelvin);
+        this.mAllObjs.addToSet(patrol);
+        
+    }
+    
+    var cannons = sceneInfo.Cannon;
+    var i, pos, cannon;
+    for (i = 0; i < cannons.length; i++) {
+        pos = cannons[i].Pos;    
+        // init cannon
+        cannon = new Cannon(this.kSprites, pos[0], pos[1], this.mKelvin, this.mAllNonPhysObj);
+        this.mAllNonPhysObj.addToSet(cannon);  
+    }
+    
+    var fliers = sceneInfo.Flier;
+    var i, pos, flier;
+    for (i = 0; i < fliers.length; i++) {
+        pos = fliers[i].Pos;    
+        // init cannon
+        flier = new Flier(this.kSprites, pos[0], pos[1], this.mKelvin, this.mAllNonPhysObj);
+        this.mAllNonPhysObj.addToSet(flier);  
+    }
+    
+    var powerups = sceneInfo.Powerup;
+    var i, pos, powerup;
+    for (i = 0; i < powerups.length; i++) {
+        pos = powerups[i].Pos;    
+        // init cannon
+        powerup = new Powerup(this.kSprites, pos[0], pos[1], this.mKelvin);
+        this.mAllNonPhysObj.addToSet(powerup);
+    }
+    
+    
+    // scene background
+    var background = sceneInfo.SceneBG[0];
+    var pos = background.Pos;
+    var size = background.Size;
+    this.mSceneBG = new TextureRenderable(this.kBG);
+    this.mSceneBG.getXform().setSize(size[0],size[1]);
+    this.mSceneBG.getXform().setPosition(pos[0],pos[1]);
+    
+    //story panels
+    var storyPanels = sceneInfo.StoryPanel;
+    var i, pos, width, txt, stubX, stubY, setText, panel;
+    for (i = 0; i < storyPanels.length; i++) {
+        pos = storyPanels[i].Pos;    
+        width = storyPanels[i].Width;
+        txt = storyPanels[i].txt;
+        stubX = storyPanels[i].stubX;
+        stubY = storyPanels[i].stubY;
+        setText = storyPanels[i].setText;
+        // init cannon
+        this.mTutoPanel = new StoryPanel(this.kWBPanel,pos[0],pos[1],width,
+        txt,stubX,stubY);
+        this.mTutoPanel.setText4(setText);
+        
+    }
+    // tutorial panel. @param(texture,atX,atY,width,txt,stubX,stubY)
+    this.mTutoPanel = new StoryPanel(this.kWBPanel,50,20,70,
+        "Story Element Panel Demo",15,3);
+    this.mTutoPanel.setText2("Space to jump. S to stomp. E to super.");
+    this.mTutoPanel.setText3("Take the health pack above.");
+    
+    
+
+
+};
+
+
+DemoScene.prototype.drawCamera = function (camera) {
+    //this.mSupport.draw(camera.getVPMatrix());
+    //this.mHero.draw(camera.getVPMatrix());
+    var i;
+    for (i = 0; i < this.mSqSet.length; i++) {
+        this.mSqSet[i].draw(camera.getVPMatrix());
+    }
+};
+
 // This is the draw function, make sure to setup proper drawing environment, and more
 // importantly, make sure to _NOT_ change any state.
-TacoDemo.prototype.draw = function () {
+DemoScene.prototype.draw = function () {
     // Step A: clear the canvas
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
     
@@ -170,7 +258,7 @@ TacoDemo.prototype.draw = function () {
 
 // The Update function, updates the application state. Make sure to _NOT_ draw
 // anything from this function!
-TacoDemo.prototype.update = function () {
+DemoScene.prototype.update = function () {
     var msg = "";
     
     // tutorial panel bounding box collision
@@ -219,11 +307,10 @@ TacoDemo.prototype.update = function () {
     msg += " CanJump status: " + collided + " |";
     msg += " Q (damage), O (Win), L (Lose)";
     this.mMsg.setText(msg);
-    
 
 };
 
-TacoDemo.prototype.createBounds = function() {
+DemoScene.prototype.createBounds = function() {
     var x = 15, w = 30, y = 0, y2 = 15;// Was 18
     for (x = 15; x < 120; x+=30)
         this.platformAt(x, y, w, 0);
@@ -233,7 +320,7 @@ TacoDemo.prototype.createBounds = function() {
 };
 
 // Make the platforms
-TacoDemo.prototype.platformAt = function (x, y, w, rot) {
+DemoScene.prototype.platformAt = function (x, y, w, rot) {
     var h = w / 8;
     var p = new TextureRenderable(this.kPlatformTexture);
     var xf = p.getXform();
@@ -253,17 +340,17 @@ TacoDemo.prototype.platformAt = function (x, y, w, rot) {
     this.mAllPlatform.addToSet(g);
 };
 // back button UI
-TacoDemo.prototype.backSelect = function(){
+DemoScene.prototype.backSelect = function(){
     this.LevelSelect="Back";
     gEngine.GameLoop.stop();
 };
 // menu button UI
-TacoDemo.prototype.mainSelect = function(){
+DemoScene.prototype.mainSelect = function(){
     this.LevelSelect="Main";
     gEngine.GameLoop.stop();
 };
 
-TacoDemo.prototype.createPipe = function(){
+DemoScene.prototype.createPipe = function(){
     var g = new TextureRenderable(this.kGreenPipe);
     var xf = g.getXform();
     xf.setSize(10,20);
@@ -281,7 +368,7 @@ TacoDemo.prototype.createPipe = function(){
     return o;
 };
 
-TacoDemo.prototype.checkWinLose = function(){
+DemoScene.prototype.checkWinLose = function(){
     // Win conditions
     var canWarp = false;
     if(this.mKelvin.getXform().getXPos() >= 93 && this.mKelvin.getXform().getXPos() <= 97 &&
@@ -303,14 +390,9 @@ TacoDemo.prototype.checkWinLose = function(){
         this.LevelSelect = "Lose";
         gEngine.GameLoop.stop();
     }
-    
-    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.J)) {
-        this.LevelSelect = "DemoScene";
-        gEngine.GameLoop.stop();
-    }
 };
 
-TacoDemo.prototype.drawMain = function() {
+DemoScene.prototype.drawMain = function() {
     this.mCamera.setupViewProjection();
     this.mSceneBG.draw(this.mCamera);
 
@@ -326,7 +408,7 @@ TacoDemo.prototype.drawMain = function() {
     this.mMsg.draw(this.mCamera);
 };
 
-TacoDemo.prototype.drawMap = function() {
+DemoScene.prototype.drawMap = function() {
     this.mMinimapCam.setupViewProjection();
 
     this.mSceneBG.draw(this.mMinimapCam);
